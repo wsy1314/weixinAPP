e868dd67b119dd44ef0ca7b4a3c2cf51d4c958f4
wsy1314 <523106472@qq.com>
2019-05-28 10:39:04 +0800

    start

---
+++ VXapp/src/main/java/io/wsy/vx20190528/Vx20190528Application.java
@@ -0,0 +1,13 @@
+package io.wsy.vx20190528;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vx20190528Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Vx20190528Application.class, args);
+    }
+
+}
